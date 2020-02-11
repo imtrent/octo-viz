b9d465bb4c3b0a9e8b224f15b4a83b8a5ee9c2e8
Ian Trent <ian.m.trent@gmail.com>
2020-02-11 18:31:09 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Landing = () => (
+    <div>
+        <Head>
+            <title>Landing</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div>
+            <p>Landing page</p>
+        </div>
+    </div>
+)
+
+export default Landing;
