162af99404fba8700490f4892ffcca54d240d9cc
Ian Trent <ian.m.trent@gmail.com>
2020-02-19 22:13:43 -0500

    Nested colors and breaks into global theme object

--- pages/_app.js
+++ pages/_app.js
@@ -2,29 +2,31 @@ import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './../styles/global';
+import './../styles/reset.css';
 
 
 const theme = {
-    primary: '#0070f3',
-    black: '#24292E',
-    lightgreen: '#2FCB53',
-    darkgreen: '#269F42',
-    mediumgray: '#404448',
-    lightgray: '#828487',
-    white: '#FFFFFF'
-}
-
-const breakpoints = {
-    small: '576px',
-    medium: '768px',
-    large: '992px'
+    colors: {
+        primary: '#0070f3',
+        black: '#24292E',
+        lightgreen: '#2FCB53',
+        darkgreen: '#269F42',
+        mediumgray: '#404448',
+        lightgray: '#828487',
+        white: '#FFFFFF'
+    },
+    breaks: {
+        small: '576px',
+        medium: '768px',
+        large: '992px'
+    }
 }
 
 export default class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props
         return (
-            <ThemeProvider theme={theme} break={breakpoints}>
+            <ThemeProvider theme={theme}>
                 <GlobalStyles />
                 <Component {...pageProps} />
             </ThemeProvider>
